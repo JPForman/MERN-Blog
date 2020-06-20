866dd915a14c06d5e135911ffee54276fdf041f7
Joshua Forman <cyborgforman@gmail.com>
2020-06-20 12:02:02 -0700

    complete blogEntry schema

---
+++ backend/models/blogEntry.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const blogEntrySchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true},
+  // img: { data: Buffer, contentType: String },
+}, {
+  timestamps: true,
+});
+
+const BlogEntry = mongoose.model('BlogEntry', blogEntrySchema);
+
+module.exports = BlogEntry;
