b5842f7dd5afde0bb3a5bf1e66513139fab9853d
Joshua Forman <cyborgforman@gmail.com>
2020-06-21 22:28:57 -0700

    add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+        <div className='collpase navbar-collapse'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='navbar-item'>
+              <Link to='/' className='nav-link'>Blog</Link>
+            </li>
+            <li className='navbar-item'>
+              <Link to='/create' className='nav-link'>Create Blog Entry</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
